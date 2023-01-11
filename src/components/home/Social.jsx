49814f70029a3d135a9f2baf7c435f70e213bcea
Low Saetern <saetern.low123@yahoo.com>
2023-01-10 17:34:10 -0700

    updated about me section and social medias

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,15 +4,6 @@ const Social = () => {
   return (
     <div className="home__social">
       <a
-        href="https://www.instagram.com/"
-        className="home__social-icon"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="uil uil-instagram"></i>
-      </a>
-
-      <a
         href="https://linkedin.com/in/lbsaetern"
         className="home__social-icon"
         target="_blank"
@@ -22,13 +13,23 @@ const Social = () => {
       </a>
 
       <a
-        href="https://github.com/"
+        href="https://github.com/lowsaetern"
         className="home__social-icon"
         target="_blank"
         rel="noreferrer"
       >
         <i className="uil uil-github-alt"></i>
       </a>
+      
+      <a
+        href="https://join.slack.com/t/lowbriansaetern-1/shared_invite/zt-1n5wwi1wx-QBTPGen~Owk~DZ~Ems7dxg"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <i className="uil uil-slack"></i>
+      </a>
+
     </div>
   );
 };
