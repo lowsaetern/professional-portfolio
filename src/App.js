0ac6c5150ceba2cadb683ff1368b8602f819174d
Low Saetern <saetern.low123@yahoo.com>
2023-01-11 12:47:16 -0700

    created contact section, deleted no longer needed components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Contact />
       </main>
     </>
     )
