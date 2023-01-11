2e934cf5c69186baf68eeed8e03380d8051a0f4d
Low Saetern <saetern.low123@yahoo.com>
2023-01-10 19:10:15 -0700

    added skills section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -63,20 +63,6 @@ const Header = () => {
 
             <li className="nav__item">
               <a
-                href="#services"
-                onClick={() => setActiveNav("#services")}
-                className={
-                  activeNav === "#services"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
-                <i className="uil uil-briefcase-alt nav__icon"></i> Services
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a
                 href="#portfolio"
                 onClick={() => setActiveNav("#portfolio")}
                 className={
