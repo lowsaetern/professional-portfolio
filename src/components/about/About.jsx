f7b28d6a8809bac9977287df92aec6198a4a1be3
Low Saetern <saetern.low123@yahoo.com>
2024-02-06 13:01:19 -0800

    fixed resume path

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Low Brian Saetern - Resume 2023.pdf";
+import CV from "../../assets/LowBrianSaetern-Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
